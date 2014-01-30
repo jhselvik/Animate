ebc2c5d7147f66572163d6f4c676113639c62ccd
Joe Selvik <joe@thenewtricks.com>
2014-01-30 12:11:13 -0600

    blue box

--- Animate/XYZViewController.m
+++ Animate/XYZViewController.m
@@ -9,7 +9,6 @@
 #import "XYZViewController.h"
 
 @interface XYZViewController ()
-@property (weak, nonatomic) IBOutlet UIView *sendMessageView;
 
 @end
 
@@ -18,9 +17,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-
-
+    _scaleFactor = 2;
+    _angle = 180;
+    CGRect frameRect = CGRectMake(10, 10, 45, 45);
+    _boxView = [[UIView alloc] initWithFrame:frameRect];
+    _boxView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:_boxView];
 }
 
 - (void)didReceiveMemoryWarning
@@ -31,18 +33,25 @@
 
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self.view];
     
-    [UIView animateWithDuration:1.0
+    [UIView animateWithDuration:2.0
                           delay:0.0
                         options:UIViewAnimationOptionCurveEaseInOut
                      animations:^{
+                         CGAffineTransform scaleTrans =
+                         CGAffineTransformMakeScale(_scaleFactor, _scaleFactor);
                          
-                         _sendMessageView.transform = CGAffineTransformMakeTranslation(0.0, -200.0);
+                         CGAffineTransform rotateTrans =
+                         CGAffineTransformMakeRotation(_angle * M_PI / 180);
                          
-
+                         _boxView.transform = CGAffineTransformConcat(scaleTrans,
+                                                                      rotateTrans);
+                         _angle = (_angle == 180 ? 360 : 180);
+                         _scaleFactor = (_scaleFactor == 2 ? 1 : 2);
+                         _boxView.center = location;
                      } completion:nil];
 }
-
 
 @end
