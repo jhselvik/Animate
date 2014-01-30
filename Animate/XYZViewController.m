99842dfbc694174503a82c538083db54e875cc65
Joe Selvik <joe@thenewtricks.com>
2014-01-30 12:22:52 -0600

    changing the blue box to slide around

--- Animate/XYZViewController.m
+++ Animate/XYZViewController.m
@@ -17,9 +17,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     _scaleFactor = 2;
     _angle = 180;
-    CGRect frameRect = CGRectMake(10, 10, 45, 45);
+    
+    CGRect frameRect = CGRectMake(100, 100, 45, 45);
     _boxView = [[UIView alloc] initWithFrame:frameRect];
     _boxView.backgroundColor = [UIColor blueColor];
     [self.view addSubview:_boxView];
@@ -33,25 +35,33 @@
 
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    UITouch *touch = [touches anyObject];
-    CGPoint location = [touch locationInView:self.view];
+    CGRect frameRect = CGRectMake(100, 200, 45, 45);
     
-    [UIView animateWithDuration:2.0
-                          delay:0.0
-                        options:UIViewAnimationOptionCurveEaseInOut
-                     animations:^{
-                         CGAffineTransform scaleTrans =
-                         CGAffineTransformMakeScale(_scaleFactor, _scaleFactor);
-                         
-                         CGAffineTransform rotateTrans =
-                         CGAffineTransformMakeRotation(_angle * M_PI / 180);
-                         
-                         _boxView.transform = CGAffineTransformConcat(scaleTrans,
-                                                                      rotateTrans);
-                         _angle = (_angle == 180 ? 360 : 180);
-                         _scaleFactor = (_scaleFactor == 2 ? 1 : 2);
-                         _boxView.center = location;
-                     } completion:nil];
+    [UIView animateWithDuration:3.0
+                     animations:^() {
+                         [self.boxView setFrame:frameRect];
+                     }];
+    
+    
+//    UITouch *touch = [touches anyObject];
+//    CGPoint location = [touch locationInView:self.view];
+//    
+//    [UIView animateWithDuration:2.0
+//                          delay:0.0
+//                        options:UIViewAnimationOptionCurveEaseInOut
+//                     animations:^{
+//                         CGAffineTransform scaleTrans =
+//                         CGAffineTransformMakeScale(_scaleFactor, _scaleFactor);
+//                         
+//                         CGAffineTransform rotateTrans =
+//                         CGAffineTransformMakeRotation(_angle * M_PI / 180);
+//                         
+//                         _boxView.transform = CGAffineTransformConcat(scaleTrans,
+//                                                                      rotateTrans);
+//                         _angle = (_angle == 180 ? 360 : 180);
+//                         _scaleFactor = (_scaleFactor == 2 ? 1 : 2);
+//                         _boxView.center = location;
+//                     } completion:nil];
 }
 
 @end
